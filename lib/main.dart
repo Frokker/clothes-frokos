48fb592cc6373dd3091c4d17e707c379b02ba598
Frokker <ilya-0609@mail.ru>
2022-12-08 16:55:24 +0500

    add documentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'dart:io';
+
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:clothes_app/app.dart';
+import 'dart:async';
 
-void main() => runApp(App());
+Future<void> main() async {
+  if (Platform.isAndroid) {
+    await AndroidAlarmManager.initialize();
+  }
+  runApp(App());
+}
